0a62befadda13b9041cd290a8f7fffa5961c6288
Rob Dimsdale <rdimsdale@pivotal.io>
2016-02-25 14:23:44 -0800

    Add runtime-ci job to validate public: true on all pipeline jobs.
    [#107466158]
    
    Signed-off-by: Nima Kaviani <nkavian@us.ibm.com>

---
+++ scripts/ci/validate-all-jobs-public/validate_jobs_public.rb
@@ -0,0 +1,18 @@
+require 'yaml'
+
+my_dir= ENV['MY_DIR']
+pipelines_dir="#{my_dir}/../../../pipelines"
+
+Dir.foreach(pipelines_dir) do |f|
+  next if File.directory?(f)
+  job_array = YAML.load_file("#{pipelines_dir}/#{f}")['jobs'].map{ |j| {:name => j['name'], :public => j['public']} }
+  job_array.each do |job|
+    RSpec.describe "pipeline jobs" do
+      context "for #{f}\##{job[:name]}" do
+        it "should have public: true" do
+          expect(job[:public]).to be_truthy
+        end
+      end
+    end
+  end
+end
