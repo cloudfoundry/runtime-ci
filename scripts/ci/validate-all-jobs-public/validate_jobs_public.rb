65efc82d5596819e85384f609fb6a5d6a7849c45
Rob Dimsdale <rdimsdale@pivotal.io>
2016-02-26 14:55:43 -0800

    Ignore cf-release when validating all jobs are public.
    [#107466158]
    
    Signed-off-by: Nima Kaviani <nkavian@us.ibm.com>

--- scripts/ci/validate-all-jobs-public/validate_jobs_public.rb
+++ scripts/ci/validate-all-jobs-public/validate_jobs_public.rb
@@ -5,6 +5,8 @@ pipelines_dir="#{my_dir}/../../../pipelines"
 
 Dir.foreach(pipelines_dir) do |f|
   next if File.directory?(f)
+  next if f =~ /cf-release.yml/
+
   job_array = YAML.load_file("#{pipelines_dir}/#{f}")['jobs'].map{ |j| {:name => j['name'], :public => j['public']} }
   job_array.each do |job|
     RSpec.describe "pipeline jobs" do
