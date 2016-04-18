09d49191fb87dcd5a47e420c3c1c9f6b5f66415f
David Sabeti <dsabeti@pivotal.io>
2016-04-18 09:54:59 -0700

    Include cf-release pipeline when checking that jobs are public
    Also includes some simplifications to the script
    
    [#115983011]
    
    Signed-off-by: Rob Dimsdale <rdimsdale@pivotal.io>

--- scripts/ci/validate-all-jobs-public/validate_jobs_public.rb
+++ scripts/ci/validate-all-jobs-public/validate_jobs_public.rb
@@ -1,13 +1,7 @@
 require 'yaml'
 
-my_dir= ENV['MY_DIR']
-pipelines_dir="#{my_dir}/../../../pipelines"
-
-Dir.foreach(pipelines_dir) do |f|
-  next if File.directory?(f)
-  next if f =~ /cf-release.yml/
-
-  job_array = YAML.load_file("#{pipelines_dir}/#{f}")['jobs'].map{ |j| {:name => j['name'], :public => j['public']} }
+Dir.glob("pipelines/*.yml").each do |f|
+  job_array = YAML.load_file(f)['jobs'].map{ |j| {:name => j['name'], :public => j['public']} }
   job_array.each do |job|
     RSpec.describe "pipeline jobs" do
       context "for #{f}\##{job[:name]}" do
