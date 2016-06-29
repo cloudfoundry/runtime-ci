039d2cf0c2dc8a5dda6b34c977df2806471e3670
David Sabeti <dsabeti@pivotal.io>
2016-06-29 11:57:45 -0700

    Add logic to configwriter to build integration_config json containing buildpack and persistent properties
    [#120646649]
    
    Signed-off-by: Dennis Leon <dleon@pivotal.io>

--- scripts/ci/run-cats/configwriter/configwriter.go
+++ scripts/ci/run-cats/configwriter/configwriter.go
@@ -6,31 +6,55 @@ import (
 )
 
 type config struct {
-	Api                  string `json:"api"`
-	AdminUser            string `json:"admin_user"`
-	AdminPassword        string `json:"admin_password"`
-	AppsDomain           string `json:"apps_domain"`
-	SkipSslValidation    bool   `json:"skip_ssl_validation"`
-	UseHttp              bool   `json:"use_http"`
-	ExistingUser         string `json:"existing_user"`
-	UseExistingUser      bool   `json:"use_existing_user"`
-	KeepUserAtSuiteEnd   bool   `json:"keep_user_at_suite_end"`
-	ExistingUserPassword string `json:"existing_user_password"`
+	Api                    string `json:"api"`
+	AdminUser              string `json:"admin_user"`
+	AdminPassword          string `json:"admin_password"`
+	AppsDomain             string `json:"apps_domain"`
+	SkipSslValidation      bool   `json:"skip_ssl_validation"`
+	UseHttp                bool   `json:"use_http"`
+	ExistingUser           string `json:"existing_user"`
+	UseExistingUser        bool   `json:"use_existing_user"`
+	KeepUserAtSuiteEnd     bool   `json:"keep_user_at_suite_end"`
+	ExistingUserPassword   string `json:"existing_user_password"`
+	StaticBuildpackName    string `json:"staticfile_buildpack_name"`
+	JavaBuildpackName      string `json:"java_buildpack_name"`
+	RubyBuildpackName      string `json:"ruby_buildpack_name"`
+	NodeJsBuildpackName    string `json:"nodejs_buildpack_name"`
+	GoBuildpackName        string `json:"go_buildpack_name"`
+	PythonBuildpackName    string `json:"python_buildpack_name"`
+	PhpBuildpackName       string `json:"php_buildpack_name"`
+	BinaryBuildpackName    string `json:"binary_buildpack_name"`
+	PersistentAppHost      string `json:"persistent_app_host"`
+	PersistentAppSpace     string `json:"persistent_app_space"`
+	PersistentAppOrg       string `json:"persistent_app_org"`
+	PersistentAppQuotaName string `json:"persistent_app_quota_name"`
 }
 
 func GenerateConfigFromEnv() config {
 	skipSslValidation, _ := strconv.ParseBool(os.Getenv("SKIP_SSL_VALIDATION"))
 	useHttp, _ := strconv.ParseBool(os.Getenv("USE_HTTP"))
 	return config{
-		Api:                  os.Getenv("CF_API"),
-		AdminUser:            os.Getenv("CF_ADMIN_USER"),
-		AdminPassword:        os.Getenv("CF_ADMIN_PASSWORD"),
-		AppsDomain:           os.Getenv("CF_APPS_DOMAIN"),
-		SkipSslValidation:    skipSslValidation,
-		UseHttp:              useHttp,
-		ExistingUser:         os.Getenv("EXISTING_USER"),
-		UseExistingUser:      os.Getenv("EXISTING_USER") != "",
-		KeepUserAtSuiteEnd:   os.Getenv("EXISTING_USER") != "",
-		ExistingUserPassword: os.Getenv("EXISTING_USER_PASSWORD"),
+		Api:                    os.Getenv("CF_API"),
+		AdminUser:              os.Getenv("CF_ADMIN_USER"),
+		AdminPassword:          os.Getenv("CF_ADMIN_PASSWORD"),
+		AppsDomain:             os.Getenv("CF_APPS_DOMAIN"),
+		SkipSslValidation:      skipSslValidation,
+		UseHttp:                useHttp,
+		ExistingUser:           os.Getenv("EXISTING_USER"),
+		UseExistingUser:        os.Getenv("EXISTING_USER") != "",
+		KeepUserAtSuiteEnd:     os.Getenv("EXISTING_USER") != "",
+		ExistingUserPassword:   os.Getenv("EXISTING_USER_PASSWORD"),
+		StaticBuildpackName:    os.Getenv("STATICFILE_BUILDPACK_NAME"),
+		JavaBuildpackName:      os.Getenv("JAVA_BUILDPACK_NAME"),
+		RubyBuildpackName:      os.Getenv("RUBY_BUILDPACK_NAME"),
+		NodeJsBuildpackName:    os.Getenv("NODEJS_BUILDPACK_NAME"),
+		GoBuildpackName:        os.Getenv("GO_BUILDPACK_NAME"),
+		PythonBuildpackName:    os.Getenv("PYTHON_BUILDPACK_NAME"),
+		PhpBuildpackName:       os.Getenv("PHP_BUILDPACK_NAME"),
+		BinaryBuildpackName:    os.Getenv("BINARY_BUILDPACK_NAME"),
+		PersistentAppHost:      os.Getenv("PERSISTENT_APP_HOST"),
+		PersistentAppSpace:     os.Getenv("PERSISTENT_APP_SPACE"),
+		PersistentAppOrg:       os.Getenv("PERSISTENT_APP_ORG"),
+		PersistentAppQuotaName: os.Getenv("PERSISTENT_APP_QUOTA_NAME"),
 	}
 }
--- scripts/ci/run-cats/configwriter/configwriter_test.go
+++ scripts/ci/run-cats/configwriter/configwriter_test.go
@@ -81,15 +81,190 @@ var _ = Describe("Configwriter", func() {
 		It("Sets 'KeepUserAtSuiteEnd' and 'UseExistingUser' to false if 'ExistingUser' is not provided", func() {
 			os.Unsetenv("EXISTING_USER")
 			config := configwriter.GenerateConfigFromEnv()
+			Expect(config).NotTo(BeNil())
 			Expect(config.UseExistingUser).To(Equal(false))
 			Expect(config.KeepUserAtSuiteEnd).To(Equal(false))
 		})
+
+		Context("When Valid Persistent App envvars are provided", func() {
+			expectedPersistedAppHost := "PERSISTENT_APP_HOST" + "_" + time.Now().String()
+			expectedPersistedAppSpace := "PERSISTENT_APP_SPACE" + "_" + time.Now().String()
+			expectedPersistedAppOrg := "PERSISTENT_APP_ORG" + "_" + time.Now().String()
+			expectedPersistedAppQuotaName := "PERSISTENT_APP_QUOTA_NAME" + "_" + time.Now().String()
+
+			BeforeEach(func() {
+				os.Setenv("PERSISTENT_APP_HOST", expectedPersistedAppHost)
+				os.Setenv("PERSISTENT_APP_SPACE", expectedPersistedAppSpace)
+				os.Setenv("PERSISTENT_APP_ORG", expectedPersistedAppOrg)
+				os.Setenv("PERSISTENT_APP_QUOTA_NAME", expectedPersistedAppQuotaName)
+			})
+			AfterEach(func() {
+				os.Unsetenv("PERSISTENT_APP_HOST")
+				os.Unsetenv("PERSISTENT_APP_SPACE")
+				os.Unsetenv("PERSISTENT_APP_ORG")
+				os.Unsetenv("PERSISTENT_APP_QUOTA_NAME")
+			})
+
+			It("Generates a config object with the correct persistent variables set", func() {
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.PersistentAppHost).To(Equal(expectedPersistedAppHost))
+				Expect(config.PersistentAppSpace).To(Equal(expectedPersistedAppSpace))
+				Expect(config.PersistentAppOrg).To(Equal(expectedPersistedAppOrg))
+				Expect(config.PersistentAppQuotaName).To(Equal(expectedPersistedAppQuotaName))
+			})
+
+			It("Does not generate a config object with PersistentAppHost if it is not set", func() {
+				os.Unsetenv("PERSISTENT_APP_HOST")
+
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.PersistentAppSpace).To(Equal(expectedPersistedAppSpace))
+				Expect(config.PersistentAppOrg).To(Equal(expectedPersistedAppOrg))
+				Expect(config.PersistentAppQuotaName).To(Equal(expectedPersistedAppQuotaName))
+
+				Expect(config.PersistentAppHost).To(Equal(""))
+			})
+
+			It("Does not generate a config object with PersistentAppSpace if it is not set", func() {
+				os.Unsetenv("PERSISTENT_APP_SPACE")
+
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.PersistentAppHost).To(Equal(expectedPersistedAppHost))
+				Expect(config.PersistentAppOrg).To(Equal(expectedPersistedAppOrg))
+				Expect(config.PersistentAppQuotaName).To(Equal(expectedPersistedAppQuotaName))
+
+				Expect(config.PersistentAppSpace).To(Equal(""))
+			})
+
+			It("Does not generate a config object with PersistentAppOrg if it is not set", func() {
+				os.Unsetenv("PERSISTENT_APP_ORG")
+
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.PersistentAppHost).To(Equal(expectedPersistedAppHost))
+				Expect(config.PersistentAppSpace).To(Equal(expectedPersistedAppSpace))
+				Expect(config.PersistentAppQuotaName).To(Equal(expectedPersistedAppQuotaName))
+
+				Expect(config.PersistentAppOrg).To(Equal(""))
+			})
+
+			It("Does not generate a config object with PersistentAppQuotaName if it is not set", func() {
+				os.Unsetenv("PERSISTENT_APP_QUOTA_NAME")
+
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.PersistentAppHost).To(Equal(expectedPersistedAppHost))
+				Expect(config.PersistentAppSpace).To(Equal(expectedPersistedAppSpace))
+				Expect(config.PersistentAppOrg).To(Equal(expectedPersistedAppOrg))
+
+				Expect(config.PersistentAppQuotaName).To(Equal(""))
+			})
+
+		})
+
+		Context("When one or more of the allowed buildpacks is provided", func() {
+			expectedStaticBuildpackName := "STATICFILE_BUILDPACK_NAME" + "_" + time.Now().String()
+			expectedJavaBuildpackName := "JAVA_BUILDPACK_NAME" + "_" + time.Now().String()
+			expectedRubyBuildpackName := "Ruby_BUILDPACK_NAME" + "_" + time.Now().String()
+			expectedNodeJsBuildpackName := "NODEJS_BUILDPACK_NAME" + "_" + time.Now().String()
+			expectedGoBuildpackName := "GO_BUILDPACK_NAME" + "_" + time.Now().String()
+
+			expectedPythonBuildpackName := "PYTHON_BUILDPACK_NAME" + "_" + time.Now().String()
+			expectedPhpBuildpackName := "PHP_BUILDPACK_NAME" + "_" + time.Now().String()
+			expectedBinaryBuildpackName := "BINARY_BUILDPACK_NAME" + "_" + time.Now().String()
+
+			BeforeEach(func() {
+				os.Setenv("STATICFILE_BUILDPACK_NAME", expectedStaticBuildpackName)
+				os.Setenv("JAVA_BUILDPACK_NAME", expectedJavaBuildpackName)
+				os.Setenv("RUBY_BUILDPACK_NAME", expectedRubyBuildpackName)
+				os.Setenv("NODEJS_BUILDPACK_NAME", expectedNodeJsBuildpackName)
+				os.Setenv("GO_BUILDPACK_NAME", expectedGoBuildpackName)
+				os.Setenv("PYTHON_BUILDPACK_NAME", expectedPythonBuildpackName)
+				os.Setenv("PHP_BUILDPACK_NAME", expectedPhpBuildpackName)
+				os.Setenv("BINARY_BUILDPACK_NAME", expectedBinaryBuildpackName)
+			})
+			AfterEach(func() {
+				os.Unsetenv("STATICFILE_BUILDPACK_NAME")
+				os.Unsetenv("JAVA_BUILDPACK_NAME")
+				os.Unsetenv("RUBY_BUILDPACK_NAME")
+				os.Unsetenv("NODEJS_BUILDPACK_NAME")
+				os.Unsetenv("GO_BUILDPACK_NAME")
+				os.Unsetenv("PYTHON_BUILDPACK_NAME")
+				os.Unsetenv("PHP_BUILDPACK_NAME")
+				os.Unsetenv("BINARY_BUILDPACK_NAME")
+			})
+
+			It("Generates a config object with the correct buildpack variables set", func() {
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.StaticBuildpackName).To(Equal(expectedStaticBuildpackName))
+				Expect(config.JavaBuildpackName).To(Equal(expectedJavaBuildpackName))
+				Expect(config.RubyBuildpackName).To(Equal(expectedRubyBuildpackName))
+				Expect(config.NodeJsBuildpackName).To(Equal(expectedNodeJsBuildpackName))
+				Expect(config.GoBuildpackName).To(Equal(expectedGoBuildpackName))
+				Expect(config.PythonBuildpackName).To(Equal(expectedPythonBuildpackName))
+				Expect(config.PhpBuildpackName).To(Equal(expectedPhpBuildpackName))
+				Expect(config.BinaryBuildpackName).To(Equal(expectedBinaryBuildpackName))
+			})
+
+			It("Does not generate a config object with StaticBuildpack if it is not set", func() {
+				os.Unsetenv("STATICFILE_BUILDPACK_NAME")
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.StaticBuildpackName).To(Equal(""))
+			})
+			It("Does not generate a config object with JavaBuildpack if it is not set", func() {
+				os.Unsetenv("JAVA_BUILDPACK_NAME")
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.JavaBuildpackName).To(Equal(""))
+			})
+			It("Does not generate a config object with RubyBuildpack if it is not set", func() {
+				os.Unsetenv("RUBY_BUILDPACK_NAME")
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.RubyBuildpackName).To(Equal(""))
+			})
+			It("Does not generate a config object with NodeJsBuildpack if it is not set", func() {
+				os.Unsetenv("NODEJS_BUILDPACK_NAME")
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.NodeJsBuildpackName).To(Equal(""))
+			})
+			It("Does not generate a config object with GoBuildpack if it is not set", func() {
+				os.Unsetenv("GO_BUILDPACK_NAME")
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.GoBuildpackName).To(Equal(""))
+			})
+			It("Does not generate a config object with PythonBuildpack if it is not set", func() {
+				os.Unsetenv("PYTHON_BUILDPACK_NAME")
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.PythonBuildpackName).To(Equal(""))
+			})
+			It("Does not generate a config object with PHPBuildpack if it is not set", func() {
+				os.Unsetenv("PHP_BUILDPACK_NAME")
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.PhpBuildpackName).To(Equal(""))
+			})
+			It("Does not generate a config object with BinaryBuildpack if it is not set", func() {
+				os.Unsetenv("BINARY_BUILDPACK_NAME")
+				config := configwriter.GenerateConfigFromEnv()
+				Expect(config).NotTo(BeNil())
+				Expect(config.BinaryBuildpackName).To(Equal(""))
+			})
+		})
 	})
 
-	It("Uses the correct keynames when marshalling to json", func() {
-		configJson, err := json.Marshal(configwriter.GenerateConfigFromEnv())
-		Expect(err).NotTo(HaveOccurred())
-		Expect(string(configJson)).To(MatchJSON(`{
+	Context("When no env variables are provided", func() {
+		It("Only renders the required keynames when marshalling to json", func() {
+			configJson, err := json.Marshal(configwriter.GenerateConfigFromEnv())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(configJson)).To(MatchJSON(`{
 																							"api": "",
 																							"admin_user": "",
 																							"admin_password": "",
@@ -99,7 +274,26 @@ var _ = Describe("Configwriter", func() {
 																							"existing_user": "",
 																							"use_existing_user": false,
 																							"keep_user_at_suite_end": false,
-																							"existing_user_password": ""
+																							"existing_user_password": "",
+																							"staticfile_buildpack_name": "",
+																							"java_buildpack_name": "",
+																							"ruby_buildpack_name": "",
+																							"nodejs_buildpack_name": "",
+																							"go_buildpack_name": "",
+																							"python_buildpack_name": "",
+																							"php_buildpack_name": "",
+																							"binary_buildpack_name": "",
+																							"persistent_app_host": "",
+																							"persistent_app_space": "",
+																							"persistent_app_org": "",
+																							"persistent_app_quota_name": ""
 																							}`))
+		})
+	})
+
+	Context("When all the env variable are provided", func() {
+		It("Renders a valid integration_config when marshalling to json", func() {
+
+		})
 	})
 })
