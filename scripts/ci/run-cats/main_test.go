78d57a99618f754823ecb3c0a30deec4fb45f07d
Dennis Leon <dleon@pivotal.io>
2016-06-28 18:02:11 -0700

    WIP on creating integration config from envs
    Signed-off-by: Jesse Alford <jalford@pivotal.io>

---
+++ scripts/ci/run-cats/main_test.go
@@ -0,0 +1,12 @@
+package main_test
+
+import (
+	. "github.com/cloudfoundry/runtime-ci/scripts/ci/run-cats"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Main", func() {
+
+})
