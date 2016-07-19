c84ffecf3ac64a105c995b989f91561e1c888084
David Sabeti <dsabeti@pivotal.io>
2016-07-19 16:13:24 -0700

    Use deep_merge in extend-bosh-lite-diego-stub
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- scripts/ci/extend-bosh-lite-diego-stub/task.rb
+++ scripts/ci/extend-bosh-lite-diego-stub/task.rb
@@ -1,17 +1,12 @@
 #!/usr/bin/env ruby
 require 'yaml'
+require 'active_support'
 
 input_filename = 'bosh-lite-stub/' + ENV.fetch('BOSH_LITE_STUB_PATH')
 output_filename = 'extended-bosh-lite-stub/' + ENV.fetch('EXTENDED_BOSH_LITE_STUB_PATH')
 
 properties = YAML.load_file(input_filename)
-properties.merge!({
-  'property_overrides' => {
-    'nsync' => {
-      'diego_privileged_containers' => true
-    }
-  }
-})
+properties.deep_merge('property_override' => {'nsync' => {'diego_privileged_containers' => true}})
 
 File.open(output_filename, 'w') do |file|
   YAML.dump(properties, file)
