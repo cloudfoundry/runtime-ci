998dd03590ccc0fce298b9c4781aea043a58092c
Joshua Aresty <joshua.aresty@emc.com>
2016-07-21 09:41:33 -0700

    Adding whitespace to look nicer

--- scripts/ci/run-cats/validationerrors/validationerrors_test.go
+++ scripts/ci/run-cats/validationerrors/validationerrors_test.go
@@ -11,13 +11,16 @@ import (
 var _ = Describe("ValidationErrors", func() {
 	It("Aggregates errors", func() {
 		ve := Errors{}
+
 		ve.Add(fmt.Errorf("error!!!!"))
 		ve.Add(fmt.Errorf("error 2!!!!"))
+
 		Expect(ve.Error()).To(And(
 			ContainSubstring("error!!!!"),
 			ContainSubstring("error 2!!!!"),
 		))
 	})
+
 	It("Knows if it is empty", func() {
 		ve := Errors{}
 		Expect(ve.Empty()).To(BeTrue())
