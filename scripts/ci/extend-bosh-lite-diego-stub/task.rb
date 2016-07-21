f2bc45e94fff516fbd2d12f72fcee811f48c2103
David Sabeti <dsabeti@pivotal.io>
2016-07-21 09:35:03 -0700

    Fix typo

--- scripts/ci/extend-bosh-lite-diego-stub/task.rb
+++ scripts/ci/extend-bosh-lite-diego-stub/task.rb
@@ -6,7 +6,7 @@ input_filename = 'bosh-lite-stub/' + ENV.fetch('BOSH_LITE_STUB_PATH')
 output_filename = 'extended-bosh-lite-stub/' + ENV.fetch('EXTENDED_BOSH_LITE_STUB_PATH')
 
 properties = YAML.load_file(input_filename)
-properties = properties.deep_merge('property_override' => {'nsync' => {'diego_privileged_containers' => true}})
+properties = properties.deep_merge('property_overrides' => {'nsync' => {'diego_privileged_containers' => true}})
 
 File.open(output_filename, 'w') do |file|
   YAML.dump(properties, file)
