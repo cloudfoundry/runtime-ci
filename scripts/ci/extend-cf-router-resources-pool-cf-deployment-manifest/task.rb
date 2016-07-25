f60a0338a4a964b7b646675cb0cb5badef1d3ce6
Josh Aresty <joshua.aresty@emc.com>
2016-07-25 14:19:44 -0700

    Change how elb is retrieved when extending the deployment manifest. (use bbl lbs instead of fetching from cloudformation)
    [#104050760]
    
    Signed-off-by: Dennis Leon <dleon@pivotal.io>

--- scripts/ci/extend-cf-router-resources-pool-cf-deployment-manifest/task.rb
+++ scripts/ci/extend-cf-router-resources-pool-cf-deployment-manifest/task.rb
@@ -13,20 +13,25 @@ def get_security_groups
   end
 end
 
-def get_elbs
-  File.open('bbl-elbs/' + ENV.fetch('BBL_ELBS_FILE'), "r") do |f|
-    f.map do |line|
-      line.split(" ")[1]
-    end
+def get_elb(name)
+  IO.popen(%(bbl --state-dir ./env-repo/#{ENV.fetch('BBL_STATE_DIRECTORY')} lbs | grep "#{name}" | cut -d: -f2 | cut -d\  -f2), "r") do |cmd|
+    return cmd.read.chomp
   end
+end
+
+def merge_resource_pool!(lb_name, resource_pool)
+  resource_pool.deep_merge!('cloud_properties' => {'security_groups' => get_security_groups })
+  resource_pool.deep_merge!('cloud_properties' => {'elbs' => get_elb(lb_name)})
 end
 
 properties = YAML.load_file(input_filename)
 
 def add_security_group_to_router(resource_pool)
-  if ["access_z1", "access_z2", "router_z1", "router_z2"].include? resource_pool["name"]
-    resource_pool.deep_merge!('cloud_properties' => {'security_groups' => get_security_groups })
-    resource_pool.deep_merge!('cloud_properties' => {'elbs' => get_elbs })
+  if ["router_z1", "router_z2"].include? resource_pool["name"]
+    merge_resource_pool!("CF Router LB", resource_pool)
+  end
+  if ["access_z1", "access_z2"].include? resource_pool["name"]
+    merge_resource_pool!("CF SSH Proxy LB", resource_pool)
   end
 end
 
