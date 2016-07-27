da1d9beea66abac5c9c33ff1727a31b43c526c86
David Sabeti <dsabeti@pivotal.io>
2016-07-26 17:12:04 -0700

    simplify extend-deployment-manifest
    Signed-off-by: Josh Aresty <joshua.aresty@emc.com>

--- scripts/ci/extend-deployment-manifest-with-security-groups-and-elb/task.rb
+++ scripts/ci/extend-deployment-manifest-with-security-groups-and-elb/task.rb
@@ -2,14 +2,14 @@
 require 'yaml'
 require 'active_support'
 
-input_filename = './generated-deployment-manifest/' + ENV.fetch('CF_DEPLOYMENT_MANIFEST')
-output_filename = './extended-cf-deployment-manifest/' + ENV.fetch('EXTENDED_CF_DEPLOYMENT_MANIFEST')
-
-def get_security_groups
-  File.open('logical-physical-id/' + ENV.fetch('CLOUDFORMATION_MAPPING_FILE'), "r") do |f|
+def aws_output_mapping
+  @aws_output_mapping ||= File.open('logical-physical-id/' + ENV.fetch('CLOUDFORMATION_MAPPING_FILE'), "r") do |f|
+    mapping = {}
     f.map do |line|
-      line.split(" ")[1]
+      key, value= line.split(" ")
+      mapping[key] = value
     end
+    mapping
   end
 end
 
@@ -19,37 +19,45 @@ def get_elb(name)
   end
 end
 
-def merge_resource_pool!(lb_name, resource_pool)
-  resource_pool.deep_merge!('cloud_properties' => {'security_groups' => get_security_groups })
-  resource_pool.deep_merge!('cloud_properties' => {'elbs' => [get_elb(lb_name)]})
-end
-
+input_filename = './generated-deployment-manifest/' + ENV.fetch('CF_DEPLOYMENT_MANIFEST')
+output_filename = './extended-cf-deployment-manifest/' + ENV.fetch('EXTENDED_CF_DEPLOYMENT_MANIFEST')
 properties = YAML.load_file(input_filename)
 
 properties.deep_merge!('properties' => {
   'template_only' => {
     'aws' => {
       'access_key_id' => ENV.fetch('AWS_ACCESS_KEY_ID'),
-      'secret_access_key' => ENV.fetch('AWS_SECRET_ACCESS_KEY')
+      'secret_access_key' => ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     }
   }
 })
 
-def add_security_group_to_router(resource_pool)
-  if ["router_z1", "router_z2"].include? resource_pool["name"]
-    merge_resource_pool!("CF Router LB", resource_pool)
-  end
-  if ["access_z1", "access_z2"].include? resource_pool["name"]
-    merge_resource_pool!("CF SSH Proxy LB", resource_pool)
-  end
+access_pools = %w[access_z1 access_z2]
+access_resource_pools = properties['resource_pools'].select do |resource_pool|
+  access_pools.include?(resource_pool['name'])
 end
 
-properties.each do | property_key, property_value |
-  if property_key == 'resource_pools'
-    property_value.each do | resource_pool_value |
-      add_security_group_to_router(resource_pool_value)
-    end
-  end
+access_resource_pools.each do |pool|
+  security_groups = [
+    aws_output_mapping['CFSSHProxyInternalSecurityGroup'],
+    aws_output_mapping['InternalSecurityGroup']
+  ]
+  pool.deep_merge!('cloud_properties' => {'security_groups' => security_groups})
+  pool.deep_merge!('cloud_properties' => {'elbs' => [get_elb('CF SSH Proxy LB')]})
+end
+
+router_pools = %w[router_z1 router_z2]
+router_resource_pools = properties['resource_pools'].select do |resource_pool|
+  router_pools.include?(resource_pool['name'])
+end
+
+router_resource_pools.each do |pool|
+  security_groups = [
+    aws_output_mapping['CFRouterInternalSecurityGroup'],
+    aws_output_mapping['InternalSecurityGroup']
+  ]
+  pool.deep_merge!('cloud_properties' => {'security_groups' => security_groups})
+  pool.deep_merge!('cloud_properties' => {'elbs' => [get_elb('CF Router LB')]})
 end
 
 File.open(output_filename, 'w') do |file|
