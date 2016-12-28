d6ea0927c54f75a04baa55013fdb25a5c1565634
Joshua Aresty <joshua.aresty@emc.com>
2016-12-27 16:22:08 -0800

    [#133463119] Initial commit of bosh lite terraform template for GCP
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

---
+++ scripts/ci/create-bosh-lite/bosh.tf
@@ -0,0 +1,76 @@
+variable "env_name" {}
+
+variable "dns_suffix" {}
+
+resource "google_dns_managed_zone" "env_dns_zone" {
+  name        = "${var.env_name}-zone"
+  dns_name    = "${var.env_name}.${var.dns_suffix}."
+  description = "DNS zone for the ${var.env_name} environment"
+}
+
+resource "google_dns_record_set" "wildcard-dns" {
+  name       = "*.${google_dns_managed_zone.env_dns_zone.dns_name}"
+  depends_on = ["google_compute_global_address.bosh-lite"]
+  type       = "A"
+  ttl        = 300
+
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
+
+  rrdatas = ["${google_compute_global_address.bosh-lite.address}"]
+}
+
+variable "projectid" {
+    type = "string"
+    default = "cf-relint-bosh-lite"
+}
+
+variable "region" {
+    type = "string"
+    default = "us-central1"
+}
+
+variable "zone" {
+    type = "string"
+    default = "us-central1-a"
+}
+
+provider "google" {
+    project = "cf-relint-bosh-lite"
+    region = "us-central1"
+}
+
+resource "google_compute_network" "bosh-lite" {
+  name       = "bosh-lite"
+}
+
+// Static IP for the BOSH director
+resource "google_compute_address" "bosh-lite" {
+  name = "bosh-lite"
+  project = "cf-relint-bosh-lite"
+  region = "us-central1"
+}
+
+// Subnet for the BOSH director
+resource "google_compute_subnetwork" "bosh-lite" {
+  name          = "bosh-lite"
+  ip_cidr_range = "10.0.0.0/16"
+  network       = "${google_compute_network.bosh-lite.self_link}"
+}
+
+// Allow ssh & mbus access to director
+resource "google_compute_firewall" "bosh-lite" {
+  name    = "bosh-lite"
+  network = "${google_compute_network.bosh-lite.name}"
+
+  allow {
+    protocol = "tcp"
+    ports = ["22", "6868", "25555", "80", "443"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["bosh-lite"]
+}
