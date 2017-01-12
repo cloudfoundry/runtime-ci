8a709daca5a84a288f0c1dde5761caab2ee34627
Adrian Zankich <azankich@pivotal.io>
2017-01-12 11:29:55 -0800

    Rename bosh-lite to bosh-lite-director in tf template
    [#133463119]
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- scripts/ci/bosh-create-env/terraform/bosh-lite/terraform.tf
+++ scripts/ci/bosh-create-env/terraform/bosh-lite/terraform.tf
@@ -27,22 +27,22 @@ variable "zone" {
     default = "us-central1-a"
 }
 
-resource "google_compute_network" "bosh-lite" {
-  name       = "bosh-lite"
+resource "google_compute_network" "bosh-lite-director" {
+  name       = "bosh-lite-director"
 }
 
 // Static IP for the BOSH director
-resource "google_compute_address" "bosh-lite" {
-  name = "bosh-lite"
+resource "google_compute_address" "bosh-lite-director" {
+  name = "bosh-lite-director"
   project = "cf-relint-bosh-lite"
   region = "us-central1"
 }
 
 // Subnet for the BOSH director
-resource "google_compute_subnetwork" "bosh-lite" {
-  name          = "bosh-lite"
+resource "google_compute_subnetwork" "bosh-lite-director" {
+  name          = "bosh-lite-director"
   ip_cidr_range = "10.0.1.0/24"
-  network       = "${google_compute_network.bosh-lite.self_link}"
+  network       = "${google_compute_network.bosh-lite-director.self_link}"
 }
 
 resource "google_dns_managed_zone" "env_dns_zone" {
@@ -53,19 +53,19 @@ resource "google_dns_managed_zone" "env_dns_zone" {
 
 resource "google_dns_record_set" "wildcard-dns" {
   name       = "*.${google_dns_managed_zone.env_dns_zone.dns_name}"
-  depends_on = ["google_compute_address.bosh-lite"]
+  depends_on = ["google_compute_address.bosh-lite-director"]
   type       = "A"
   ttl        = 300
 
   managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
 
-  rrdatas = ["${google_compute_address.bosh-lite.address}"]
+  rrdatas = ["${google_compute_address.bosh-lite-director.address}"]
 }
 
 // Allow ssh & mbus access to director
-resource "google_compute_firewall" "bosh-lite" {
-  name    = "bosh-lite"
-  network = "${google_compute_network.bosh-lite.name}"
+resource "google_compute_firewall" "bosh-lite-director" {
+  name    = "bosh-lite-director"
+  network = "${google_compute_network.bosh-lite-director.name}"
 
   allow {
     protocol = "tcp"
@@ -82,23 +82,23 @@ resource "google_compute_firewall" "bosh-lite" {
 
 // Outputs go below here:
 output "external_ip" {
-  value = "${google_compute_address.bosh-lite.address}"
+  value = "${google_compute_address.bosh-lite-director.address}"
 }
 
 output "network_name" {
-  value = "${google_compute_network.bosh-lite.name}"
+  value = "${google_compute_network.bosh-lite-director.name}"
 }
 
 output "subnetwork_name" {
-  value = "${google_compute_subnetwork.bosh-lite.name}"
+  value = "${google_compute_subnetwork.bosh-lite-director.name}"
 }
 
 output "internal_cidr" {
-  value = "${google_compute_subnetwork.bosh-lite.ip_cidr_range}"
+  value = "${google_compute_subnetwork.bosh-lite-director.ip_cidr_range}"
 }
 
 output "internal_gw" {
-  value = "${google_compute_subnetwork.bosh-lite.gateway_address}"
+  value = "${google_compute_subnetwork.bosh-lite-director.gateway_address}"
 }
 
 output "internal_ip" {
