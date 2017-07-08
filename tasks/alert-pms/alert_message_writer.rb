99fdefddb1fa328e32fea9b656234ea03e58f1eb
David Sabeti <dsabeti@pivotal.io>
2017-07-07 17:18:24 -0700

    Alert message writer prunes empty handles from the message

--- tasks/alert-pms/alert_message_writer.rb
+++ tasks/alert-pms/alert_message_writer.rb
@@ -8,7 +8,18 @@ class AlertMessageWriter
     pm_github, anchor_github = team_section.split("\r\n")[1].gsub(':', '').split(', ')
     team = @release_teams.find_team_by_github_handles(anchor_github, pm_github)
     open(File.join("slack-messages", team.name), 'w') do |file|
-      file.puts "Hey there <#{team.pm_slack}> <#{team.anchor_slack}>. Could you please take a look at the latest release candidate: #{@issue_url} cc <@dsabeti>"
+      file.puts build_message(team)
     end
+  end
+
+  private
+
+  def build_message(team)
+    message = "Could you please take a look at the latest release candidate: #{@issue_url} cc <@dsabeti>"
+    greeting = "Hey there"
+    addressees = ""
+    addressees << "<#{team.pm_slack}>"
+    addressees << " <#{team.anchor_slack}>" unless team.anchor_slack.nil? || team.anchor_slack.empty?
+    "#{greeting} #{addressees}. #{message}"
   end
 end
