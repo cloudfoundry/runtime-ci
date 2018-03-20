3d260e3313aa353bbb55a9dc18646089458a4482
David Sabeti <dsabeti@pivotal.io>
2018-03-20 14:47:19 -0700

    Add describe block to test

--- tasks/cf-deployment-release-notes-template/renderer_spec.rb
+++ tasks/cf-deployment-release-notes-template/renderer_spec.rb
@@ -25,41 +25,42 @@ describe 'Renderer' do
       updates
     end
 
-
-    it 'includes a header' do
-      expect(renderer.render(release_updates: release_updates)).to include(
+    describe 'Release and stemcell table' do
+      it 'includes a header' do
+        expect(renderer.render(release_updates: release_updates)).to include(
 <<-HEADER
 | Release | New Version | Old Version |
 | ------- | ----------- | ----------- |
 HEADER
-      )
-    end
-
-    it 'shows the release name, new version, and old version for each release' do
-      rendered_output = renderer.render(release_updates: release_updates)
-      expect(rendered_output).to include('| release-1 | 1.3.0 | 1.1.0 |')
-      expect(rendered_output).to include('| release-2 | 1.4.0 | 1.2.0 |')
-    end
-
-    context 'when some versions are nil' do
-      let(:release_update_1) do
-        update = double('ReleaseUpdate')
-        allow(update).to receive(:old_version) { '1.1.0' }
-        allow(update).to receive(:new_version) { nil }
-        update
+        )
       end
 
-      let(:release_update_2) do
-        update = double('ReleaseUpdate')
-        allow(update).to receive(:old_version) { nil }
-        allow(update).to receive(:new_version) { '1.4.0' }
-        update
-      end
-
-      it 'renders them as empty strings' do
+      it 'shows the release name, new version, and old version for each release' do
         rendered_output = renderer.render(release_updates: release_updates)
-        expect(rendered_output).to include('| release-1 |  | 1.1.0 |')
-        expect(rendered_output).to include('| release-2 | 1.4.0 |  |')
+        expect(rendered_output).to include('| release-1 | 1.3.0 | 1.1.0 |')
+        expect(rendered_output).to include('| release-2 | 1.4.0 | 1.2.0 |')
+      end
+
+      context 'when some versions are nil' do
+        let(:release_update_1) do
+          update = double('ReleaseUpdate')
+          allow(update).to receive(:old_version) { '1.1.0' }
+          allow(update).to receive(:new_version) { nil }
+          update
+        end
+
+        let(:release_update_2) do
+          update = double('ReleaseUpdate')
+          allow(update).to receive(:old_version) { nil }
+          allow(update).to receive(:new_version) { '1.4.0' }
+          update
+        end
+
+        it 'renders them as empty strings' do
+          rendered_output = renderer.render(release_updates: release_updates)
+          expect(rendered_output).to include('| release-1 |  | 1.1.0 |')
+          expect(rendered_output).to include('| release-2 | 1.4.0 |  |')
+        end
       end
     end
   end
