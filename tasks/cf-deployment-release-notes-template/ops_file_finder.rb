ee57539aa45ef2de53d075fada6cda1304db27f1
Alexander Berezovsky <aberezovsky@pivotal.io>
2019-03-25 11:12:47 -0700

    Include all ops subfolders except legacy and workaround
    [#163463466](https://www.pivotaltracker.com/story/show/163463466)
    
    Signed-off-by: Dave Walter <dwalter@pivotal.io>

--- tasks/cf-deployment-release-notes-template/ops_file_finder.rb
+++ tasks/cf-deployment-release-notes-template/ops_file_finder.rb
@@ -1,13 +1,11 @@
 class OpsFileFinder
   def self.find_ops_files(repo_dir)
-    ops_files_and_directories = Dir.glob(File.join(repo_dir, "operations", "*.yml"))
-    opsfile_list = ops_files_and_directories.select { |file_or_directory| File.file?(file_or_directory) }
+    folders_to_exclude = %r{/legacy/|/workaround/}
+    ops_files_and_directories = Dir.glob(
+      File.join(repo_dir, 'operations', '**', '*.yml')
+    ).grep_v folders_to_exclude
 
-    experimental_ops_files = Dir.glob(File.join(repo_dir, "operations", "experimental", "*.yml"))
-    opsfile_list += experimental_ops_files
-
-    experimental_ops_files = Dir.glob(File.join(repo_dir, "operations", "addons", "*.yml"))
-    opsfile_list += experimental_ops_files
+    opsfile_list = ops_files_and_directories.select { |fd| File.file?(fd) }
 
     opsfile_list.map { |opsfile| opsfile.gsub!("#{repo_dir}/operations/", '') }
   end
--- tasks/cf-deployment-release-notes-template/ops_file_finder_spec.rb
+++ tasks/cf-deployment-release-notes-template/ops_file_finder_spec.rb
@@ -2,73 +2,98 @@ require 'rspec'
 require_relative './ops_file_finder.rb'
 
 describe 'OpsFileFinder' do
+  subject { OpsFileFinder.find_ops_files('test-cf-deployment') }
+
   before(:all) do
     @current_work_dir = Dir.pwd
     @tmp_work_dir = Dir.mktmpdir('test-cf-deployment')
 
     Dir.chdir(@tmp_work_dir)
-
-    FileUtils.mkdir_p('test-cf-deployment')
-    FileUtils.mkdir_p('test-cf-deployment/operations/experimental')
-    FileUtils.mkdir_p('test-cf-deployment/operations/addons')
+    FileUtils.mkdir_p('test-cf-deployment/operations')
   end
 
   after(:all) do
     Dir.chdir(@current_work_dir)
-    FileUtils.rm_rf(@tmp_work_dir) if File.exists?(@tmp_work_dir)
+    FileUtils.rm_rf(@tmp_work_dir) if File.exist?(@tmp_work_dir)
   end
 
   context 'when there are ops-files in the lop-level operations directory' do
     before do
-      File.open('test-cf-deployment/operations/ops.yml', 'w') {}
-      File.open('test-cf-deployment/operations/ops2.yml', 'w') {}
-      File.open('test-cf-deployment/operations/README.md', 'w') {}
+      File.open('test-cf-deployment/operations/ops.yml', 'w')
+      File.open('test-cf-deployment/operations/ops2.yml', 'w')
+      File.open('test-cf-deployment/operations/README.md', 'w')
     end
 
     it 'returns the file names of the ops-files without any additional path' do
-      ops = OpsFileFinder.find_ops_files('test-cf-deployment')
-
-      expect(ops).to include 'ops.yml'
-      expect(ops).to include 'ops2.yml'
+      expect(subject).to include 'ops.yml'
+      expect(subject).to include 'ops2.yml'
     end
 
     it 'does not return any files that are not yaml files' do
-      ops = OpsFileFinder.find_ops_files('test-cf-deployment')
-
-      expect(ops).not_to include 'README.md'
+      expect(subject).not_to include 'README.md'
     end
   end
 
   context 'when there is another directory in the operations directory' do
     before do
-      File.open('test-cf-deployment/operations/experimental/ops.yml', 'w') {}
-      File.open('test-cf-deployment/operations/experimental/ops2.yml', 'w') {}
-      File.open('test-cf-deployment/operations/addons/ops.yml', 'w') {}
-      File.open('test-cf-deployment/operations/addons/ops2.yml', 'w') {}
+      FileUtils.mkdir_p("test-cf-deployment/operations/#{subfolder}")
+      File.open("test-cf-deployment/operations/#{subfolder}/ops.yml", 'w')
+      File.open("test-cf-deployment/operations/#{subfolder}/ops2.yml", 'w')
     end
 
-    it 'does not include the directory in the list of files' do
-      ops = OpsFileFinder.find_ops_files('test-cf-deployment')
+    let(:expect_ops_files_to_be_included) do
+      expect(subject).to include "#{subfolder}/ops.yml"
+      expect(subject).to include "#{subfolder}/ops2.yml"
+    end
 
-      expect(ops).not_to include 'experimental'
-      expect(ops).not_to include 'addons'
+    let(:expect_ops_files_to_be_excluded) do
+      expect(subject).not_to include "#{subfolder}/ops.yml"
+      expect(subject).not_to include "#{subfolder}/ops2.yml"
+    end
+
+    let(:subfolder) { 'nested' }
+
+    it 'does not include the directory in the list of files' do
+      expect(subject).not_to include 'nested'
     end
 
     context 'and the directory is the experimental directory' do
-      it 'returns the ops-files prepended with "experimentlal"' do
-        ops = OpsFileFinder.find_ops_files('test-cf-deployment')
+      let(:subfolder) { 'experimental' }
 
-        expect(ops).to include 'experimental/ops.yml'
-        expect(ops).to include 'experimental/ops2.yml'
+      it 'returns the ops-files prepended with "experimentlal"' do
+        expect_ops_files_to_be_included
       end
     end
 
     context 'and the directory is the addons directory' do
-      it 'returns the ops-files prepended with "addons"' do
-        ops = OpsFileFinder.find_ops_files('test-cf-deployment')
+      let(:subfolder) { 'addons' }
 
-        expect(ops).to include 'addons/ops.yml'
-        expect(ops).to include 'addons/ops2.yml'
+      it 'returns the ops-files prepended with "addons"' do
+        expect_ops_files_to_be_included
+      end
+    end
+
+    context 'and the directory is the legacy directory' do
+      let(:subfolder) { 'legacy' }
+
+      it 'does not return the ops-files prepended with "legacy"' do
+        expect_ops_files_to_be_excluded
+      end
+    end
+
+    context 'and the directory is the workaround directory' do
+      let(:subfolder) { 'workaround' }
+
+      it 'does not return the ops-files prepended with "workaround"' do
+        expect_ops_files_to_be_excluded
+      end
+    end
+
+    context 'directory ends with .yml' do
+      let(:subfolder) { 'yaml-dir.yml' }
+
+      it 'does not include the directory in a list of files' do
+        expect(subject).not_to include 'yaml-dir.yml'
       end
     end
   end
