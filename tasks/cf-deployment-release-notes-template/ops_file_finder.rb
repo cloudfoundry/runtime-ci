2f3d037d3842c7a6031d7b163963e733f0bb146e
Andrew Costa <acosta@pivotal.io>
2019-08-27 09:56:48 -0700

    Revert change to cf-deployment-release-notes-template
    - we accidentally broke the extraction of release bumps from ops-files
    
    [#168144738](https://www.pivotaltracker.com/story/show/168144738)
    
    Co-authored-by: Dave Walter <dwalter@pivotal.io>

--- tasks/cf-deployment-release-notes-template/ops_file_finder.rb
+++ tasks/cf-deployment-release-notes-template/ops_file_finder.rb
@@ -2,7 +2,7 @@ class OpsFileFinder
   def self.find_ops_files(repo_dir)
     folders_to_exclude = %r{/workaround/}
     ops_files_and_directories = Dir.glob(
-      File.join(repo_dir, '*', 'task.yml')
+      File.join(repo_dir, 'operations', '**', '*.yml')
     ).grep_v folders_to_exclude
 
     opsfile_list = ops_files_and_directories.select { |fd| File.file?(fd) }
