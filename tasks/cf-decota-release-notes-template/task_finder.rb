0cdc87aa3dc124a9688d4acf471d67c98597c3f3
Weyman Fung <wfung@pivotal.io>
2019-11-01 14:39:03 -0700

    move cf-d-c-t specific tasks to that repo
    [#169512901](https://www.pivotaltracker.com/story/show/169512901)

--- tasks/cf-decota-release-notes-template/task_finder.rb
+++
@@ -1,9 +0,0 @@
-class TaskFinder
-  def self.find_tasks(repo_dir)
-    tasks = Dir.glob(
-      File.join(repo_dir, '*', 'task.yml')
-    )
-    task_list = tasks.select { |fd| File.file?(fd) }
-    task_list.map { |task| task.gsub!("#{repo_dir}/", '').gsub!("/task.yml", '') }
-  end
-end
